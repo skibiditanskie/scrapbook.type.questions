118c1f99697697ae3470dc680e127f8f5de12c47
skibiditanskie <khanhbeo512@gmail.com>
2024-11-06 16:21:45 +0800

    Add files via upload

---
+++ ScrapbookTypeQuestions.java
@@ -0,0 +1,92 @@
+
+package scrapbook.type.questions;
+
+import java.util.Scanner;
+
+public class ScrapbookTypeQuestions {
+
+    public static void main(String[] args) {
+
+                Scanner scanner ;
+                scanner  = new Scanner (System.in); 
+                
+    String Favhobby = "Gaming" ;
+    
+    System.out.print("What is your Favorite hobby: ");
+    Favhobby = scanner.nextLine();
+        
+    int age = 19 ; 
+    
+    System.out.print("How old are you?:") ;
+    age = scanner.nextInt(); 
+         
+    int country = 1 ;
+    
+    System.out.print("How many country have i visited?: ") ;
+    country = scanner.nextInt() ;
+    
+    int favnumber = 14 ;
+    
+    System.out.print("What is my favorite number?: ") ;
+    favnumber = scanner.nextInt();
+    
+    String favmovie = "Saving Private ryan" ;
+    
+    System.out.print("What is my movie: ");
+    favmovie = scanner.nextLine();
+    
+    String word = "Introvert" ;
+    
+    System.out.print("What is the one word that discribes me: ");
+    word = scanner.nextLine();
+        
+    int adventurous = 1 ;
+    
+    System.out.print("In a scale of 1-10, how adventurous are you?: ");
+    adventurous = scanner.nextInt() ;
+    
+    String titleofbook = "Something Wicked This Way Comes" ;
+    
+    System.out.print("What is the book title that i love?: "); 
+    titleofbook = scanner.nextLine() ;
+    
+    String dreamcountry = "South Korea" ;
+    
+    System.out.print("What is my dream travel destination?: ") ;
+    dreamcountry = scanner.nextLine() ;
+    
+    String skill = "Dancing" ;
+    
+    System.out.print("What is the skill that i like to learn?: ") ;
+    skill = scanner.nextLine();
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
